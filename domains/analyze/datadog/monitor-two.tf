2493a7327fb7411e109a0931f0e9f421c2c99b33
caique <77406850+corqueeuvia@users.noreply.github.com>
2024-10-04 12:01:07 -0300

    Revert "remove all data from all files and delete image (#47)" (#50)
    This reverts commit 628da6b4db2e4815759ae21eb4b409c0e48b1069.

--- domains/analyze/datadog/monitor-two.tf
+++ domains/analyze/datadog/monitor-two.tf
@@ -1,1 +1,17 @@
-> All data removed
+resource "datadog_monitor" "foo2" {
+  name               = "Name for monitor foo2"
+  type               = "metric alert"
+  message            = "Monitor triggered. Notify: @hipchat-channel"
+  escalation_message = "Escalation message @pagerduty"
+
+  query = "avg(last_1h):avg:aws.ec2.cpu{environment:foo,host:foo2} by {host} > 4"
+
+  monitor_thresholds {
+    warning  = 28
+    critical = 48
+  }
+
+  include_tags = true
+
+  tags = ["foo:bar", "team:fooBar2"]
+}
